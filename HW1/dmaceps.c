eac1073e06090f50a97661b69f3ef8f59d583611
gbmitchell <gbmitchell@gmail.com>
2018-09-03 16:44:57 -0600

    Update dmaceps.c

--- HW1/dmaceps.c
+++ HW1/dmaceps.c
@@ -28,21 +28,3 @@ void dmaceps(double *seps, int *ipow) {
 
 	return;
 }
-
-int main() {
-
-	// create and initialize arguments
-	double seps = 1.0;
-	int ipow = 0;
-
-	// call dmaceps function passing arguments by reference
-	dmaceps(&seps, &ipow);
-
-	// print the resulting values to the console
-	printf("\n%d\t%.8e", ipow, seps);
-
-	// keeps the console open until a key is pressed
-	getch();
-
-	return 0;
-}
