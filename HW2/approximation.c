73e792c5c06d63d09b139731af1e89bdac7538a8
gbmitchell <gbmitchell@gmail.com>
2018-09-18 07:39:25 -0600

    added source files

---
+++ HW2/approximation.c
@@ -0,0 +1,332 @@
+#include <stdio.h>
+#include <math.h>
+#include <mylib.h>
+
+
+int main() {
+	
+	double Xn1 = 0.0;
+
+	
+
+	int i = 0;
+	int ipow = 0;
+	int  maxIteration = 0;
+	
+	double eps = 0.0;
+	double eabsolute = 0.0;
+	double erelative = 0.0;
+
+	double x = 0.0;
+	double h = 0.0;
+	double expder = 0.0;
+	double fxx = 0.0;
+	
+	double root = 0.0;
+	double exproot = 0.0;
+
+	double initialGuess = 0.0;
+	double initialGuess1 = 0.0;
+
+	double Pi = 3.1415926535897932384626433832795028841971693993751058209749445923;
+	
+
+	//***************************************************************************************
+	//*******************  approximation d/dx(x^2 + 3) at x = 2.0  **************************
+	//***************************************************************************************
+	x = 2.0;
+	h = 1.0;
+	maxIteration = 55;
+	expder = 4.0;
+	dmaceps(&eps, &ipow);
+	
+	printf("approximation for d/dx(x^2 + 3)\n");
+
+	fxx = expderivative(fnct1, x, h, eps, maxIteration, expder);
+
+		
+
+	//***************************************************************************************
+	//*******************  approximation for d/dx(e^x) at x = 2.0  **************************
+	//***************************************************************************************
+	x = 2.0;
+	h = 1.0;
+	maxIteration = 55;
+	expder = 7.38905609893;
+	dmaceps(&eps, &ipow);
+
+	printf("approximation for d/dx(e^x)\n");
+	
+	fxx = expderivative(exp, x, h, eps, maxIteration, expder);
+
+
+	//***************************************************************************************
+	//*******************  approximation for d/dx( sqrt(x) ) at x = 2.0  ********************
+	//***************************************************************************************
+	x = 2.0;
+	h = 1.0;
+	maxIteration = 50;
+	expder = 0.353553390593;
+	dmaceps(&eps, &ipow);
+
+	printf("approximation for d/dx( sqrt(x) )\n");
+
+	fxx = expderivative(sqrt, x, h, eps, maxIteration, expder);
+
+
+	//***************************************************************************************
+	//************* alternative approximation for d/dx( sqrt(x) ) at x = 2.0  ***************
+	//***************************************************************************************
+	x = 2.0;
+	h = 1.0;
+	maxIteration = 55;
+	expder = 0.353553390593;
+	dmaceps(&eps, &ipow);
+
+	i = 0;
+	eabsolute = 0.0;
+	erelative = 0.0;
+
+	printf("alternate approximation for d/dx( sqrt(x) )\n");
+	printf("dmaceps =  %.8e\n", eps);
+	printf("x = %lf\n", x);
+	printf("expected derivative = %.12lf\n\n", expder);
+
+	printf("i\t| appoximate derivative\t\t| h\t\t| e absolute\t| e relative\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	eabsolute = 10.0 * eps;
+
+	while ((eabsolute > eps) && maxIteration > 0) {
+
+		maxIteration--;
+		i++;
+
+		fxx = ((x + h) - x) / (h*(sqrt(x + h) + sqrt(x)));
+
+		eabsolute = eabs(fxx, expder);
+		erelative = erel(fxx, expder);
+
+		printf("%d\t| %.8e\t\t| %.3e\t| %.3e\t| %.3e\n", i, fxx, h, eabsolute, erelative);
+		h = h / 2.0;
+
+	}
+	printf("\n\n\n");
+
+
+	//***************************************************************************************
+	//************* direct use of d/dx( sqrt(x) ) =  1 / (2 * sqrt(x)), at x = 2.0  *********
+	//***************************************************************************************
+
+	expder = 0.353553390593;
+	x = 2.0;
+	dmaceps(&eps, &ipow);
+
+	printf("direct use of d/dx( sqrt(x) ) = 1 / (2 * sqrt(x))\n");
+	printf("dmaceps =  %.8e\n", eps);
+	printf("x = %lf\n", x);
+	printf("expected derivative = %.12lf\n\n", expder);
+
+	printf("direct derivative\t\t| e absolute\t| e relative\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	fxx = 1 / (2 * sqrt(x));
+
+	eabsolute = eabs(fxx, expder);
+	erelative = erel(fxx, expder);
+
+	printf("%.8e\t\t\t| %.3e\t| %.3e\n", fxx, eabsolute, erelative);
+
+	printf("\n\n\n");
+
+
+	//***************************************************************************************
+	//************* approximation for d/dx( sin(x) ) at x = 0.0  ***************
+	//***************************************************************************************
+	x = 0.0;
+	h = 1.0;
+	maxIteration = 50;
+	expder = 1.0;
+	dmaceps(&eps, &ipow);
+
+	printf("approximation for d/dx( sin(x) )\n");
+
+	fxx = expderivative(sin, x, h, eps, maxIteration, expder);
+
+
+	//***************************************************************************************
+	//************* taylor series approximation for d/dx( sin(x) ) at x = 0.0  **************
+	//************************** f(x) = x - (x^3/6) *****************************************
+	//***************************************************************************************
+	x = 0.0;
+	h = 1.0;
+	maxIteration = 50;
+	expder = 1.0;
+	dmaceps(&eps, &ipow);
+
+	printf("taylor series approximation for d/dx( sin(x) ) = d/dx( x - ( x^3/6 ) )\n");
+
+	fxx = expderivative(fnct2, x, h, eps, maxIteration, expder);
+
+
+	//***************************************************************************************
+	//************ root finding fixed point iteration for x^2 - 3 ***************************
+	//***************************************************************************************
+
+	initialGuess = 2;
+	i = 0;
+	maxIteration = 85;
+	exproot = 1.73205080757;
+	dmaceps(&eps, &ipow);
+	eabsolute = 1.0;
+
+	printf("fixed point approximation for f(x) = ( x^2 - 3 ), where g(x) = x - ( f(x)/10 )\n");
+	printf("dmaceps =  %.8e\n", eps);
+	printf("Initial value = %lf\n", initialGuess);
+	printf("expected root = %.8lf\n\n", exproot);
+	printf("i\t| Xn\t\t\t| Xn+1\t\t\t| e absolute\t| e relative\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	while ((eabsolute > eps) && (maxIteration > 0) ) {
+
+		maxIteration--;
+		i++;
+
+		Xn1 = fixed2(fnct8, initialGuess, eps, maxIteration);
+
+		eabsolute = eabs(Xn1, exproot);
+		erelative = erel(Xn1, exproot);
+
+		printf("%d\t| %.8e\t| %.8e\t| %.3e\t| %.3e\n", i, initialGuess, Xn1, eabsolute, erelative);
+
+		initialGuess = Xn1;
+	}
+	printf("\n\n\n");
+
+
+	//*****************************************************************************************
+	//* root finding fixed point iteration method 1 for sin(Pi*x), roots at n = 0, 1, 2, ...  *
+	//*****************************************************************************************
+
+	dmaceps(&eps, &ipow);
+	initialGuess = 0.5;
+	i = 0;
+	maxIteration = 100;
+	exproot = 1.0;
+	eabsolute = 1.0;
+
+	printf("fixed point approximation for f(x) = sin(Pi*x), where g(x) = x + ( f(x)/10 )\n");
+	printf("dmaceps =  %.8e\n", eps);
+	printf("Initial value = %.1lf\n", initialGuess);
+	printf("expected root = %.8lf\n\n", exproot);
+	printf("i\t| Xn\t\t\t| Xn+1\t\t\t|  e absolute\t| e relative\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	while ( (eabsolute > eps) && (maxIteration > 0) ) {
+		
+		maxIteration--;
+		i++;
+
+		Xn1 = fixed1(fnct9, initialGuess, eps, maxIteration);
+
+		eabsolute = eabs(Xn1, exproot);
+		erelative = erel(Xn1, exproot);
+
+		printf("%d\t| %.8e\t| %.8e\t| %.3e\t| %.3e\n", i, initialGuess, Xn1, eabsolute, erelative);
+
+		initialGuess = Xn1;
+
+	}
+
+	printf("\n\n\n");
+
+
+
+	//***************************************************************************************
+	//********************* root finding bisection method for x^2 - 3 ***********************
+	//***************************************************************************************
+	initialGuess = 0.0;
+	initialGuess1 = 100.0;
+	exproot = sqrt(3);
+	dmaceps(&eps, &ipow);
+	maxIteration = bisMaxItr(initialGuess, initialGuess1, eps);
+
+	printf("bisection method approximation for f(x) = ( x^2 - 3 )\n");
+
+	root = expbisection(fnct4, initialGuess, initialGuess1, eps, maxIteration, exproot);
+
+	
+
+	//***************************************************************************************
+	//********************* root finding bisection method for sin(Pi * x) *******************
+	//***************************************************************************************
+	initialGuess = 1.75;
+	initialGuess1 = 2.25;
+	exproot = 2.0;
+	dmaceps(&eps, &ipow);
+	maxIteration = bisMaxItr(initialGuess, initialGuess1, eps);
+
+	printf("bisection method approximation for f(x) = sin(Pi * x)\n");
+
+	root = expbisection(fnct6, initialGuess, initialGuess1, eps, maxIteration, exproot);
+
+
+
+	//***************************************************************************************
+	//*******************  root finding newtons method for x^2 - 3   ************************
+	//***************************************************************************************
+	initialGuess = 2.0;
+	exproot = sqrt(3);
+	maxIteration = 10;
+	dmaceps(&eps, &ipow);
+
+	printf("newton method approximation for f(x) = ( x^2 - 3 )\n");
+
+	root = expnewton(fnct4, fnct5, initialGuess, eps, maxIteration, exproot);
+
+
+
+
+	//***************************************************************************************
+	//*******************  root finding newtons method for sin(Pi * x) **********************
+	//***************************************************************************************
+	initialGuess = 6.2;
+	exproot = 6.0;
+	maxIteration = 10;
+	dmaceps(&eps, &ipow);
+	
+	printf("newton method approximation for f(x) = sin(Pi * x)\n");
+
+	root = expnewton(fnct6, fnct7, initialGuess, eps, maxIteration, exproot);
+
+
+	//***************************************************************************************
+	//*******************  root finding secant method for x^2 - 3   *************************
+	//***************************************************************************************
+	initialGuess = 3.0;
+	initialGuess1 = 6.0;
+	exproot = sqrt(3);
+	maxIteration = 10;
+	dmaceps(&eps, &ipow);
+
+	printf("secant method approximation for f(x) = ( x^2 - 3 )\n");
+
+	root = expsecant(fnct4, initialGuess, initialGuess1, eps, maxIteration, exproot);
+
+
+
+	//***************************************************************************************
+	//*******************  root finding secant method for sin(Pi * x) ***********************
+	//***************************************************************************************
+	initialGuess = 3.2;
+	initialGuess1 = 3.3;
+	exproot = 3.0;
+	maxIteration = 10;
+	dmaceps(&eps, &ipow);
+
+	printf("secant method approximation for f(x) = sin(Pi * x)\n");
+
+	root = expsecant(fnct6, initialGuess, initialGuess1, eps, maxIteration, exproot);
+
+	return 0;
+}
---
+++ HW2/mylib.c
@@ -0,0 +1,507 @@
+#include <stdio.h>
+#include <math.h>
+#include "mylib.h"
+
+double Pi = 3.1415926535897932384626433832795028841971693993751058209749445923;
+
+void smaceps(float *seps, int *ipow) {
+
+	// create an initialize function variables
+	// initialized to find machine value near 1.0
+	float one = 0.0, appone = 0.0;
+	int i = 0;
+	one = 1.0;
+	*seps = 1.0;
+	appone = one + *seps;
+	*ipow = 0;
+
+	// loop, dividing by 2 each time to determine when the difference
+	//  between one and the approximation is zero in single precision
+	for (i = 0; i < 1000; i++) {
+		*ipow = *ipow + 1;
+		*seps = *seps / 2.0;
+		appone = one + *seps;
+		if (fabs(appone - one) == 0.0) return;
+	}
+
+	// print error message to console if loops more than 1000 times
+	// code should never reach this point unless there is an error
+	printf("The loop limit has been exceeded");
+
+	return;
+}
+
+void dmaceps(double *seps, int *ipow) {
+
+	// create an initialize function variables
+	// initialized to find machine value near 1.0
+	double one = 0.0, appone = 0.0;
+	int i = 0;
+	one = 1.0;
+	*seps = 1.0;
+	appone = one + *seps;
+	*ipow = 0;
+
+	// loop, dividing by 2 each time to determine when the difference
+	//  between one and the approximation is zero in double precision
+	for (i = 0; i < 1000; i++) {
+		*ipow = *ipow + 1;
+		*seps = *seps / 2.0;
+		appone = one + *seps;
+		if (fabs(appone - one) == 0.0) return;
+	}
+
+	// print error message to console if loops more than 1000 times
+	// code should never reach this point unless there is an error
+	printf("The loop limit has been exceeded");
+
+	return;
+}
+
+double eabs(double v, double u) {
+	double e = fabs(u - v);
+	return e;
+}
+
+double erel(double v, double u) {
+	double e = (fabs(u- v)) / (fabs(u));
+	return e;
+}
+
+// funciton x^2 + 3
+double fnct1(double x) {
+
+	double fx = 0.0;
+	fx = (x * x) + 3;
+	return fx;
+}
+
+
+// function taylor series sin(x)
+double fnct2(double x) {
+
+	double fx = 0.0;
+	fx = x - ((x * x * x) / 6);
+	return fx;
+}
+
+// function taylor series d/dx( sin(x) )
+double fnct3(double x) {
+
+	double fx = 0.0;
+	fx = 1 - ((x * x) / 2);
+	return fx;
+}
+
+// function x^2 - 3
+double fnct4(double x) {
+
+	double fx = 0.0;
+	fx = (x * x) - 3;
+	return fx;
+}
+
+// function d/dx( x^2 - 3 )
+double fnct5(double x) {
+
+	double fx = 0.0;
+	fx = 2 * x;
+	return fx;
+}
+
+// function ( sin(Pi * x) )
+double fnct6(double x) {
+
+	double fx = 0.0;
+	fx = sin(Pi * x);
+	return fx;
+}
+
+// function d/dx( sin(Pi * x) )
+double fnct7(double x) {
+
+	double fx = 0.0;
+	fx = Pi * cos(Pi * x);
+	return fx;
+}
+
+// function ( x^2 - 3 )/10
+double fnct8(double x) {
+
+	double fx = 0.0;
+	fx = ( (x * x) - 3) / 10.0;
+	return fx;
+}
+
+// function sin(Pi * x)/10
+double fnct9(double x) {
+
+	double fx = 0.0;
+	fx = ( sin(Pi * x) ) / 10.0;
+	return fx;
+}
+
+int bisMaxItr(double a, double b, double tol) {
+
+	int x = ((log(tol / fabs(b - a)) / log(2)) / (log(0.5) / log(2))) + 1;
+	return x;
+}
+
+
+double fixed1(double(*f)(double), double xn) {
+
+	double fxn1 = 0.0;
+	fxn1 = xn + f(xn);
+	return fxn1;
+}
+
+
+double fixed2(double(*f)(double), double xn) {
+
+	double fxn1 = 0.0;
+	fxn1 = xn - f(xn);
+	return fxn1;
+}
+
+
+double expderivative(double(*f)(double), double x, double h, double tol, int maxItr, double expder) {
+
+	int i = 0;
+	double fp = 0.0;
+	double eabsolute = 0.0;
+	double erelative = 0.0;
+
+	printf("dmaceps =  %.8e\n", tol);
+	printf("x = %lf\n", x);
+	printf("expected derivative = %.12lf\n\n", expder);
+
+	printf("i\t| appoximate derivative\t\t| h\t\t| e absolute\t| e relative\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	eabsolute = 10.0 * tol;
+
+	while ((eabsolute > tol) && maxItr > 0) {
+
+		maxItr--;
+		i++;
+
+		fp = (f(x + h) - f(x)) / (h);
+
+		eabsolute = eabs(fp, expder);
+		erelative = erel(fp, expder);
+
+		printf("%d\t| %.8e\t\t| %.3e\t| %.3e\t| %.3e\n", i, fp, h, eabsolute, erelative);
+		h = h / 2.0;
+
+	}
+	printf("\n\n\n");
+
+	return fp;
+}
+
+
+double expbisection(double(*f)(double), double a, double b, int tol, int maxItr, double exproot) {
+	int i = 0;
+	double c = 0.0;
+	double Fxc = 1.0;
+	double Fxa = f(a);
+	double Fxb = f(b);
+	double root = 0.0;
+	double eabsolute = 0.0;
+	double erelative = 0.0;
+
+	printf("max itterations = %d, for tollerence = %.8e\n", maxItr, tol);
+	printf("Lower initial value = %.1lf, Upper initial value = %.1lf\n\n", a, b);
+	printf("expected root = %.12lf\n\n", exproot);
+	printf("i\t| root\t\t\t| e absolute\t| e relative\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	if ((Fxa * Fxb) < 0.0) {
+
+		while (maxItr > 0) {
+
+			maxItr--;
+			i++;
+
+			c = (a + b) / 2.0;
+
+			Fxc = f(c);
+
+			if ((Fxa * Fxc) < 0.0) {
+				b = c;
+				Fxb = f(b);
+			}
+			else if ((Fxa * Fxc) > 0.0) {
+				a = c;
+				Fxa = f(a);
+			}
+			else {
+				eabsolute = eabs(c, exproot);
+				erelative = erel(c, exproot);
+				printf("%d\t| %.8e\t| %.3e\t| %.3e\n", i, c, eabsolute, erelative);
+				break;
+			}
+			eabsolute = eabs(c, exproot);
+			erelative = erel(c, exproot);
+			printf("%d\t| %.8e\t| %.3e\t| %.3e\n", i, c, eabsolute, erelative);
+		}
+
+		root = c;
+
+	}
+	else if ((Fxa * Fxb) == 0.0) {
+
+		if (Fxa == 0) {
+			root = a;
+			eabsolute = eabs(a, exproot);
+			erelative = erel(a, exproot);
+			printf("%d\t| %.8e\t| %.3e\t| %.3e\n", i, a, eabsolute, erelative);
+		}
+		if (Fxb == 0) {
+			root = b;
+			eabsolute = eabs(b, exproot);
+			erelative = erel(b, exproot);
+			printf("%d\t| %.8e\t| %.3e\t| %.3e\n", i, b, eabsolute, erelative);
+		}
+
+	}
+	else {
+
+		printf("\n\nThere is no root or an even number of roots between the upper and lower initial values.");
+		root = 0.0;
+	}
+
+	printf("\n\n\n");
+
+	return root;
+}
+
+
+double expnewton(double(*f)(double), double(*fp)(double), double x, double tol, int maxItr, double exproot) {
+
+	int i = 0;
+	double fxn1 = 0.0;
+	double eabsolute = 0.0;
+	double erelative = 0.0;
+
+	printf("dmaceps =  %.8e\n", tol);
+	printf("Initial value = %lf\n", x);
+	printf("expected root = %.12lf\n\n", exproot);
+	printf("i\t| root\t\t\t| e absolute\t| e relative\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	eabsolute = 10.0 * tol;
+
+	while ((eabsolute > tol) && maxItr > 0) {
+
+		maxItr--;
+		i++;
+
+		fxn1 = x - (f(x)) / (fp(x));
+
+		eabsolute = eabs(fxn1, exproot);
+		erelative = erel(fxn1, exproot);
+
+		printf("%d\t| %.8e\t| %.3e\t| %.3e\n", i, fxn1, eabsolute, erelative);
+
+		x = fxn1;
+	}
+
+	printf("\n\n\n");
+
+
+	return fxn1;
+}
+
+
+double expsecant(double(*f)(double), double xa, double xb, double tol, int maxItr, double exproot) {
+
+	int i = 0;
+	double fxn1 = 0.0;
+	double eabsolute = 0.0;
+	double erelative = 0.0;
+
+	printf("dmaceps =  %.8e\n", tol);
+	printf("Initial values = %lf and %lf\n", xa, xb);
+	printf("expected root = %.12lf\n\n", exproot);
+
+	printf("i\t| root\t\t\t| e absolute\t| e relative\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	eabsolute = 10.0 * tol;
+
+	while ((eabsolute > tol) && maxItr > 0) {
+
+		maxItr--;
+		i++;
+
+
+		fxn1 = xb - ((f(xb) * (xb - xa)) / (f(xb) - f(xa)));
+
+		eabsolute = eabs(fxn1, exproot);
+		erelative = erel(fxn1, exproot);
+
+		printf("%d\t| %.8e\t| %.3e\t| %.3e\n", i, fxn1, eabsolute, erelative);
+
+		xa = xb;
+		xb = fxn1;
+
+	}
+
+	printf("\n\n\n");
+
+	return fxn1;
+}
+
+
+double derivative(double(*f)(double), double x, double h, int maxItr) {
+
+	int i = 0;
+	double fp = 0.0;
+
+	printf("x = %lf\n", x);
+
+	printf("i\t| appoximate derivative\t\t| h\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	while (maxItr > 0) {
+
+		maxItr--;
+		i++;
+
+		fp = (f(x + h) - f(x)) / (h);
+
+		printf("%d\t| %.8e\t\t| %.3e\n", i, fp, h);
+		h = h / 2.0;
+
+	}
+
+	printf("\n\n\n");
+
+	return fp;
+}
+
+
+double bisection(double(*f)(double), double a, double b, double tol, int maxItr) {
+	int i = 0;
+	double c = 0.0;
+	double Fxc = 1.0;
+	double Fxa = f(a);
+	double Fxb = f(b);
+	double root = 0.0;
+
+	printf("max itterations = %d, for tollerence = %.8e\n", maxItr, tol);
+	printf("Lower initial value = %.1lf, Upper initial value = %.1lf\n\n", a, b);
+	printf("i\t| root\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	if ((Fxa * Fxb) < 0.0) {
+
+		while (maxItr > 0) {
+
+			maxItr--;
+			i++;
+
+			c = (a + b) / 2.0;
+
+			Fxc = f(c);
+
+			if ((Fxa * Fxc) < 0.0) {
+				b = c;
+				Fxb = f(b);
+			}
+			else if ((Fxa * Fxc) > 0.0) {
+				a = c;
+				Fxa = f(a);
+			}
+			else {
+				printf("%d\t| %.8e\n", i, c);
+				break;
+			}
+			printf("%d\t| %.8e\n", i, c);
+		}
+
+		root = c;
+
+	}
+	else if ((Fxa * Fxb) == 0.0) {
+
+		if (Fxa == 0) {
+			root = a;
+			printf("%d\t| %.8e\n", i, a);
+		}
+		if (Fxb == 0) {
+			root = b;
+			printf("%d\t| %.8e\n", i, b);
+		}
+
+	}
+	else {
+
+		printf("\n\nThere is no root or an even number of roots between the upper and lower initial values.");
+		root = 0.0;
+	}
+
+	printf("\n\n\n");
+
+	return root;
+}
+
+
+double newton(double(*f)(double), double(*fp)(double), double x, double tol, int maxItr) {
+
+	int i = 0;
+	double fxn1 = 0.0;
+
+	printf("dmaceps =  %.8e\n", tol);
+	printf("Initial value = %lf\n", x);
+	printf("i\t| root\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	while (maxItr > 0) {
+
+		maxItr--;
+		i++;
+
+		fxn1 = x - (f(x)) / (fp(x));
+
+		printf("%d\t| %.8e\n", i, fxn1);
+
+		x = fxn1;
+	}
+
+	printf("\n\n\n");
+
+	return fxn1;
+}
+
+
+double secant(double(*f)(double), double xa, double xb, double tol, int maxItr) {
+
+	int i = 0;
+	double fxn1 = 0.0;
+
+	printf("dmaceps =  %.8e\n", tol);
+	printf("Initial values = %lf and %lf\n", xa, xb);
+
+	printf("i\t| root\n");
+	printf("--------------------------------------------------------------------------------------------\n");
+
+	while (maxItr > 0) {
+
+		maxItr--;
+		i++;
+
+		fxn1 = xb - ((f(xb) * (xb - xa)) / (f(xb) - f(xa)));
+
+		printf("%d\t| %.8e\n", i, fxn1);
+
+		xa = xb;
+		xb = fxn1;
+
+	}
+
+	printf("\n\n\n");
+
+	return fxn1;
+}
